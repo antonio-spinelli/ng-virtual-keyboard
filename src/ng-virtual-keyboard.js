2ad9105057ffe0eb34d1f77dcd528a589628068f
Antonio Spinelli <lordfong86-git@yahoo.it>
2015-10-03 12:12:43 +0200

    Added methods to the service for get the keyboard from a element reference

--- src/ng-virtual-keyboard.js
+++ src/ng-virtual-keyboard.js
@@ -1,46 +1,55 @@
 angular.module('ng-virtual-keyboard', [])
 
 .constant('VKI_CONFIG', {
-	
+
 })
 
 .service('ngVirtualKeyboardService', ['VKI_CONFIG', function(VKI_CONFIG) {
-	var clone = function(obj) {
-	    var copy;
+	var clone = function(obj){
+			var copy;
 
-	    // Handle the 3 simple types, and null or undefined
-	    if (null == obj || "object" != typeof obj) return obj;
+			// Handle the 3 simple types, and null or undefined
+			if (null == obj || "object" != typeof obj) return obj;
 
-	    // Handle Date
-	    if (obj instanceof Date) {
-	        copy = new Date();
-	        copy.setTime(obj.getTime());
-	        return copy;
-	    }
+			// Handle Date
+			if (obj instanceof Date) {
+					copy = new Date();
+					copy.setTime(obj.getTime());
+					return copy;
+			}
 
-	    // Handle Array
-	    if (obj instanceof Array) {
-	        copy = [];
-	        for (var i = 0, len = obj.length; i < len; i++) {
-	            copy[i] = clone(obj[i]);
-	        }
-	        return copy;
-	    }
+			// Handle Array
+			if (obj instanceof Array) {
+					copy = [];
+					for (var i = 0, len = obj.length; i < len; i++) {
+							copy[i] = clone(obj[i]);
+					}
+					return copy;
+			}
 
-	    // Handle Object
-	    if (obj instanceof Object) {
-	        copy = {};
-	        for (var attr in obj) {
-	            if (obj.hasOwnProperty(attr)) copy[attr] = clone(obj[attr]);
-	        }
-	        return copy;
-	    }
+			// Handle Object
+			if (obj instanceof Object) {
+					copy = {};
+					for (var attr in obj) {
+							if (obj.hasOwnProperty(attr)) copy[attr] = clone(obj[attr]);
+					}
+					return copy;
+			}
 
-	    throw new Error("Unable to copy obj! Its type isn't supported.");
+			throw new Error("Unable to copy obj! Its type isn't supported.");
+	};
+
+	var executeGetKeyboard = function(elementReference){
+		var keyboard;
+		var element = $(elementReference);
+		if (element) {
+			keyboard = $(elementReference).getkeyboard();
+		}
+		return keyboard;
 	};
 
 	return {
-		attach: function(element, config, inputCallback) {
+		attach: function(element, config, inputCallback){
 			var newConfig = clone(VKI_CONFIG);
 
 			config = config || {};
@@ -52,6 +61,12 @@ angular.module('ng-virtual-keyboard', [])
 			newConfig.accepted = config.accepted || inputCallback;
 
 			$(element).keyboard(newConfig);
+		},
+		getKeyboard: function(elementReference){
+			return executeGetKeyboard(elementReference);
+		},
+		getKeyboardById: function(id){
+			return executeGetKeyboard('#' + id);
 		}
 	};
 }])
